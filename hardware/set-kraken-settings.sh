341d0b92ecdaf60a2669906b57c0c64d4b3dbe4e
Jonas Malaco <jonas@protocubo.io>
2018-09-19 14:57:25 -0300

    Hardware: also configure the Smart Device while we are at it

--- hardware/set-kraken-settings.sh
+++ hardware/set-kraken-settings.sh
@@ -1,33 +1,54 @@
 #!/bin/bash -x
 
-# Configure the Kraken X62
+# Configure my Kraken X62 and Smart Device
 #
-# Besides propagating error codes, uses the ring LEDs to convey to the user, in
+# Besides propagating error codes, uses the LEDs to convey to the user, in
 # a visual way,  progression and eventual failures.  This makes the script
 # suitable for unattained operation, such as automated configuration during
 # boot.
-#
-# However, the settings listed here are for my particular setup, and should
-# only be used as reference, if that.  Even so, they consider a Kraken X62 as
-# top exaust, with NF-A14s fans in the pull configuration, and a happy 8700K.
 
-function panic() {
-	set code=$?
-	liquidctl --device 0 set ring color pulse ff2608 --speed faster
+
+KRAKEN="--device 0"
+# The settings listed here are for my particular setup, and should only be used
+# as reference, if that.  Even so, they consider a Kraken X62 as top exaust,
+# with NF-A14s fans in the pull configuration, and a happy 8700K.
+
+function kraken_panic() {
+	code=$?
+	liquidctl $KRAKEN set ring color pulse ff2608 --speed faster
 	exit $code
 }
 
-liquidctl --device 0 set sync color super af5a2f 000000 000000 000000 000000 000000 000000 000000 000000 || panic
+liquidctl $KRAKEN set sync color super-fixed af5a2f || kraken_panic
 
-# compensate for increased liquid temperature from gpu
-liquidctl --device 0 set fan speed 22 30 36 90 40 100 || panic
-liquidctl --device 0 set ring color super 000000 af5a2f af5a2f af5a2f 000000 000000 000000 000000 000000 || panic
+liquidctl $KRAKEN set fan speed 30 60 36 90 40 100 || kraken_panic
+liquidctl $KRAKEN set ring color super-fixed 000000 af5a2f af5a2f af5a2f || kraken_panic
 
-# the benefits of running the pump above 60% are almost imperceptible
-liquidctl --device 0 set pump speed 60 || panic
-liquidctl --device 0 set ring color fixed af5a2f || panic
+liquidctl $KRAKEN set pump speed 30 60 36 90 40 100 || kraken_panic
+liquidctl $KRAKEN set ring color fixed af5a2f || kraken_panic
 
-# discretely let the user know that all is well
 sleep 1
-liquidctl --device 0 set ring color covering-marquee af5a2f || panic
+liquidctl $KRAKEN set ring color covering-marquee af5a2f || kraken_panic
+
+
+SMART_DEVICE="--device 1"
+
+function smart_device_panic() {
+	code=$?
+	liquidctl $SMART_DEVICE set led color pulse 500804 --speed faster
+	exit $code
+}
+
+liquidctl $SMART_DEVICE reset || smart_device_panic
+
+for i in {1..2};
+do
+	liquidctl $SMART_DEVICE set fan$i speed 40 || smart_device_panic
+done
+
+liquidctl $SMART_DEVICE set led color off || smart_device_panic
+
+
+sleep 10;
+liquidctl status
 
