5c4bdf4790678d8b8a2dd0f25318837e76bd0e8d
Jonas Malaco <jonas@protocubo.io>
2023-02-19 12:05:10 -0300

    fish: add abbr py for python

--- fish/config.fish
+++ fish/config.fish
@@ -11,6 +11,7 @@ if status is-interactive
     abbr -a jo xdg-open
     abbr -a l ls -lah
     abbr -a md mkdir -p
+    abbr -a py python
     abbr -a t1 tree -L 1
     abbr -a t2 tree -L 2
     abbr -a t3 tree -L 3
