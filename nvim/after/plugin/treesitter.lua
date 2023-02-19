96f6f66a49f3e292dec6510e01628682525aee19
Jonas Malaco <jonas@protocubo.io>
2023-02-19 06:03:05 -0300

    nvim: add todo about sill unconfigured treesitter features

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,3 +1,5 @@
+-- TODO: configure additional fitures like syntax-aware selection, moves, indents, etc.
+
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
   ensure_installed = {
