44fdade8537945dc5760bc1bbfc05ed8a16a4a9c
Jonas Malaco <jonas@protocubo.io>
2023-03-01 21:38:51 -0300

    fish: always fully shutdown v4l2phone

--- fish/functions/v4l2phone_shutdown.fish
+++ fish/functions/v4l2phone_shutdown.fish
@@ -1,6 +1,6 @@
 function v4l2phone_shutdown -d "Shutdown the phone camera V4L2 device"
     # Cleanup
     adb forward --remove tcp:9009
-    and sudo modprobe -r v4l2loopback
-    and adb kill-server
+    sudo modprobe -r v4l2loopback
+    adb kill-server
 end
