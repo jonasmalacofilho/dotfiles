011b75474ddbd5ec5498b4eac17e3f90b3bb4c33
Jonas Malaco <jonas@protocubo.io>
2023-03-13 11:13:40 -0300

    fish: add [w]atch and [w]atch[e]xec abbrs

--- fish/config.fish
+++ fish/config.fish
@@ -17,6 +17,8 @@ if status is-interactive
     abbr -a t2 tree -L 2
     abbr -a t3 tree -L 3
     abbr -a v nvim
+    abbr -a w watch --differences --interval 1
+    abbr -a we watchexec --clear
 
     abbr -a icat kitty +kitten icat
     abbr -a scat kitty +kitten ssh
