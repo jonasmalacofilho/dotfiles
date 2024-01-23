0de4216accf0b3e55698c9b5c6f20be7969f6fe3
Jonas Malaco <jonas@protocubo.io>
2024-01-23 03:37:25 -0300

    fish: add --accept-no and --prompt to confirm function

--- fish/functions/confirm.fish
+++ fish/functions/confirm.fish
@@ -1,13 +1,24 @@
-# TODO: support --prompt-str [and, correspondingly, --]
 function confirm -d "Request user confirmation before executing \$argv"
-    switch (read --prompt-str="Are you sure you want to continue? (y/N) ")
+    argparse --ignore-unknown "accept-no" "prompt=" -- $argv
+    or return
+
+    if test -z "$_flag_prompt"
+        set _flag_prompt "Are you sure you want to continue?"
+    end
+
+    read --prompt-str="$_flag_prompt (y/N) " -l response
+    or return 1 # if the read was aborted with ctrl-c/ctrl-d
+
+    switch $response
         case y Y
             $argv
         case n N ''
-            echo Aborted
-            return 2
+            if test -z "$_flag_accept_no"
+                echo "Aborted"
+                return 2
+            end
         case '*'
-            echo Invalid answer
+            echo "Invalid answer"
             return 1
     end
 end
