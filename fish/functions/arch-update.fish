690af237d2d69d10b9fc494e31d134d48c65d63d
Jonas Malaco <jonas@protocubo.io>
2024-01-22 22:50:10 -0300

    fish: add --threeway and --backup to arch-update -> pacdiff
    pacdiff --sudo could be slightly better (one less sudo plus my neovim
    config would be avaible), but it doesn't seem to work with --threaway.

--- fish/functions/arch-update.fish
+++ fish/functions/arch-update.fish
@@ -1,4 +1,5 @@
 function arch-update -d "Update Arch system with pacman"
+    # pacdiff requires neovim-symlinks or neovim-drop-in or vim[diff]
     sudo pacman -Syu
-    and sudo pacdiff # REQUIRES: neovim-symlinks, neovim-drop-in or vim[diff]
+    and sudo pacdiff --threeway --backup
 end
