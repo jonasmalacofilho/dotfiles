725c56adf800637f6732a2f988e4a2184fa6253e
Jonas Malaco <jonas@protocubo.io>
2024-01-23 03:38:31 -0300

    fish: add yay-update

---
+++ fish/functions/yay-update.fish
@@ -0,0 +1,12 @@
+function yay-update -d "Update AUR/yay packages with yay"
+    confirm --accept-no --prompt "Clean yay cache first?" -- sudo yay -Sca
+    or return
+
+    echo -e "\nUpgrading AUR/yay packages"
+    yay -Sua
+    or return 1
+
+    # pacdiff requires neovim-symlinks or neovim-drop-in or vim[diff]
+    echo -e "\nChecking for pacnew files"
+    sudo pacdiff --threeway --backup
+end
