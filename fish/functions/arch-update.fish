a3e962deb4ebe261a7dfa54d0a3ef9360d03dc2c
Jonas Malaco <jonas@protocubo.io>
2024-01-22 23:03:04 -0300

    fish: offer to clean pacman and yay's caches during arch-update

--- fish/functions/arch-update.fish
+++ fish/functions/arch-update.fish
@@ -1,5 +1,20 @@
 function arch-update -d "Update Arch system with pacman"
-    # pacdiff requires neovim-symlinks or neovim-drop-in or vim[diff]
+    read --nchars 1 -l response --prompt-str="Clean pacman and yay's caches first? (y/N)"
+    or return  1
+    switch $response
+        case y Y
+            sudo yay -Sc
+            or return 1
+        case n N ''
+            # noop
+        case '*'
+            echo Invalid answer
+            return 1
+    end
+
     sudo pacman -Syu
-    and sudo pacdiff --threeway --backup
+    or return 1
+
+    # pacdiff requires neovim-symlinks or neovim-drop-in or vim[diff]
+    sudo pacdiff --threeway --backup
 end
