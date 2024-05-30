2321a9cb81bf5ae349b70cb6dfeb4e35258199b7
Jonas Malaco <jonas@protocubo.io>
2024-05-30 05:36:28 -0300

    kickstart.nvim: highlight TODO/FIXME/etc. outside of comments too

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -685,7 +685,10 @@ require('lazy').setup({
     'folke/todo-comments.nvim',
     event = 'VimEnter',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
+    opts = {
+      signs = false,
+      highlight = { comments_only = false },
+    },
   },
 
   -- Adds git related signs to the gutter, as well as utilities for managing changes.
