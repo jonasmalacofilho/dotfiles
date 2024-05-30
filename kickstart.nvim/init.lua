c8c4d22574c0a8e3a29310a28059cb406a10bb78
Jonas Malaco <jonas@protocubo.io>
2024-05-30 05:27:10 -0300

    kickstart.nvim: add binding to search including hiddne/ignored files

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -648,6 +648,10 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      vim.keymap.set('n', '<leader>sF', function()
+        builtin.find_files { hidden = true, no_ignore = true }
+      end, { desc = '[S]earch All [F]iles' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
