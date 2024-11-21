3e1b600890539e683958c4f1cafe7b716e5df462
Jonas Malaco <jonas@protocubo.io>
2024-11-21 07:10:01 -0300

    nvim: add workaround for rust_analyzer -32802 error
    I'm lazy today and decided not to fiddle with my LspAttach hook for
    this workaround. And everything seems to work.
    
    Link: https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
    Link: https://github.com/praveenperera/dotfiles/commit/3cd2a6d3c1cc

---
+++ nvim/after/ftplugin/rust.lua
@@ -0,0 +1,9 @@
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
