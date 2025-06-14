03b14bae3f422172849fe87b7a5de781bd194f63
Jonas Malaco <jonas@protocubo.io>
2025-06-14 06:09:58 -0300

    nvim: copy custom diagnostic configuration but keep using the defaults

--- nvim/init.lua
+++ nvim/init.lua
@@ -412,6 +412,36 @@ require('lazy').setup({
         end,
       })
 
+      -- Diagnostic Config.
+      -- See :help vim.diagnostic.Opts
+      -- Taken from kickstart.nvim but disabled because, as is, it's too noisy.
+      -- vim.diagnostic.config {
+      --   severity_sort = true,
+      --   float = { border = 'rounded', source = 'if_many' },
+      --   underline = { severity = vim.diagnostic.severity.ERROR },
+      --   signs = vim.g.have_nerd_font and {
+      --     text = {
+      --       [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      --       [vim.diagnostic.severity.WARN] = '󰀪 ',
+      --       [vim.diagnostic.severity.INFO] = '󰋽 ',
+      --       [vim.diagnostic.severity.HINT] = '󰌶 ',
+      --     },
+      --   } or {},
+      --   virtual_text = {
+      --     source = 'if_many',
+      --     spacing = 2,
+      --     format = function(diagnostic)
+      --       local diagnostic_message = {
+      --         [vim.diagnostic.severity.ERROR] = diagnostic.message,
+      --         [vim.diagnostic.severity.WARN] = diagnostic.message,
+      --         [vim.diagnostic.severity.INFO] = diagnostic.message,
+      --         [vim.diagnostic.severity.HINT] = diagnostic.message,
+      --       }
+      --       return diagnostic_message[diagnostic.severity]
+      --     end,
+      --   },
+      -- }
+
       -- LSP servers and clients are able to communicate to each other what features they support.
       -- By default, Neovim doesn't support everything that is in the LSP specification. When you
       -- add blink.cmp, luasnip, etc. Neovim now has *more* capabilities. So, in theory, we need to
