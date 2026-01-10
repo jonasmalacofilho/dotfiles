202d6f29e90e9e0cb8c6e16af2d99fc6e08831a9
Jonas Malaco <jonas@protocubo.io>
2026-01-10 14:32:04 -0300

    fish: switch to starfish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -101,4 +101,8 @@ if status is-interactive
         source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
         set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
     end
+
+    if type -q starship
+        starship init fish | source
+    end
 end
