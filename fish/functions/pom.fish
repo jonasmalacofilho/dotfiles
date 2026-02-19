516a40a36cc4552708ffa65cd0d66ef95dd78683
Jonas Malaco <jonas@protocubo.io>
2026-02-19 01:45:43 -0300

    fish: loop in pom until manually exited

--- fish/functions/pom.fish
+++ fish/functions/pom.fish
@@ -6,26 +6,22 @@
 # Adapted from: https://gist.github.com/bashbunni/e311f07e100d51a883ab0414b46755fa
 
 function pom
-    set split $POMO_SPLIT
-    if ! test -n "$split"
-        set split $(gum choose "25/5" "50/10" "all done" --header "Choose a pomodoro split:")
+    while true
+        switch $(gum choose "25/5" "50/10" "all done" --header "Choose a pomodoro split:")
+        case 25/5
+            set work 25m
+            set break 5m
+        case 50/10
+            set work 50m
+            set break 10m
+        case "*"
+            return
+        end
+
+        termdown $work && kitten notify -u critical "Pomodoro" "Work timer is up! Take a break 😊"
+
+        gum confirm "Ready for a break?" \
+            && termdown $break \
+            && kitten notify -u critical "Pomodoro" "Break is over! Get back to work 😬"
     end
-
-    switch $split
-    case 25/5
-        set work 25m
-        set break 5m
-    case 50/10
-        set work 50m
-        set break 10m
-    case "*"
-        return
-    end
-
-    termdown $work && kitten notify -u critical "Pomodoro" "Work Timer is up! Take a break 😊"
-
-    gum confirm "Ready for a break?" \
-        && termdown $break \
-        && kitten notify -u critical "Pomodoro" "Break is over! Get back to work 😬" \
-        || pom
 end
