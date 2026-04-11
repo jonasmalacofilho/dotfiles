34f3cc042d891a124c1c6cd1fbc0b63c472fe6ec
Jonas Malaco <jonas@protocubo.io>
2026-04-11 13:33:54 -0300

    nvim: remove unused old stuff from old kickstart.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -828,16 +828,6 @@ require('lazy').setup({
     end,
   },
 
-  -- TODO: tpope/vim-eunuch?
-
-  -- Additional Kickstart plugins available.
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns keymaps
-
   -- Load more plugins, config, etc.
   -- See: `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'plugins' },
